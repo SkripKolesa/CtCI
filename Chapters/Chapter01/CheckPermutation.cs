f4fc7b844ed4e6ac48691f449b04d3be3bafeeed
artem.novikov <biokako@gmail.com>
2019-10-29 21:40:59 +0400

    CheckPermutation 1st working idea

--- Chapters/Chapter01/CheckPermutation.cs
+++ Chapters/Chapter01/CheckPermutation.cs
@@ -9,7 +9,18 @@ namespace Chapters.Chapter01
     {
         public static bool IsPermutationOf(this string a, string b)
         {
-            throw new NotImplementedException();
+            if (b == null) return false;
+            if (a.Length != b.Length) return false;
+            var aArr = a.ToCharArray();
+            var bArr = b.ToCharArray();
+            Array.Sort(aArr);
+            Array.Sort(bArr);
+            for (int i = 0; i < aArr.Length; i++)
+            {
+                if (aArr[i] != bArr[i]) return false;
+            }
+
+            return true;
         }
     }
 }
