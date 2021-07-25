b29eab4f55fed7d9906e6d68597545bb98a3cd13
artem.novikov <biokako@gmail.com>
2021-07-25 16:34:31 +0400

    empty implementation

---
+++ Chapters.Tests/Chapter01/StringCompressionTests.cs
@@ -0,0 +1,15 @@
+﻿using Chapters.Chapter01;
+using Xunit;
+
+namespace Chapters.Tests.Chapter01
+{
+    public class StringCompressionTests
+    {
+        [Theory]
+        [InlineData("aabccccaaa", "a2bc5a3")]
+        public void TestCompress(string str, string compressed)
+        {
+            Assert.Equal(str.Compress(), compressed);
+        }
+    }
+}
---
+++ Chapters/Chapter01/StringCompression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapters.Chapter01
+{
+    /// <summary>
+    /// Implement a method to perform basic string compression using the counts of repeated characters.
+    /// For example, the string aabccccaaa would become a2bc5a3. If the "compressed" string would not become
+    /// smaller then the original string, your method should return the original string.
+    /// You can assume the string has only uppercase and lowercase letters (a-z).
+    /// </summary>
+    public static class StringCompression
+    {
+        public static string Compress(this string str)
+        {
+            throw new NotImplementedException("Compress");
+        }
+    }
+}
