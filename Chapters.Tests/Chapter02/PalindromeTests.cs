0f8056d6287107cc01582b200e96ff850bafe1fd
artem.novikov <biokako@gmail.com>
2024-05-12 18:25:45 +0400

    Palindrome bolvanka

---
+++ Chapters.Tests/Chapter02/PalindromeTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Shared.LinkedLists;
+using Xunit;
+
+namespace Chapters.Tests.Chapter02;
+
+public class PalindromeTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 2, 1 })]
+    public void DetectsPalindromes(int[] input)
+    {
+        var head = NodeHelper.FromEnumerable(input);
+        throw new NotImplementedException();
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 })]
+    public void DetectsNonPalindromes(int[] input)
+    {
+        var head = NodeHelper.FromEnumerable(input);
+        throw new NotImplementedException();
+    }
+}
---
+++ Chapters/Chapter02/Palindrome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Shared.LinkedLists;
+
+namespace Chapters.Chapter02;
+
+public static partial class Solutions
+{
+    public static bool IsPalindrome<T>(Node<T> head)
+    {
+        throw new NotImplementedException();
+    }
+}
