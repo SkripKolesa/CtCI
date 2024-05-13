2cf1e36eadf700c7c79e8e16809dea8aad6503f5
artem.novikov <biokako@gmail.com>
2024-05-14 00:08:38 +0400

    2.7 intersection bolvanka

---
+++ Chapters.Tests/Chapter02/IntersectionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Chapters.Chapter02;
+using Shared.LinkedLists;
+using Xunit;
+
+namespace Chapters.Tests.Chapter02;
+
+public class IntersectionTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 }, new[] { -1, -2, -3 }, new[] { 10, 20, 30 })]
+    public void DetectsIntersections(int[] startA, int[] startB, int[] commonPart)
+    {
+        var headA = NodeHelper.FromEnumerable(startA);
+        var headB = NodeHelper.FromEnumerable(startB);
+        var headCommon = NodeHelper.FromEnumerable(commonPart);
+        NodeHelper.AppendToTail(headA, headCommon);
+        NodeHelper.AppendToTail(headB, headCommon);
+
+        var actual = Solutions.GetIntersectionNode(headA, headB);
+        Assert.Same(headCommon, actual);
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 }, new[] { 1, 2, 3 })]
+    public void DetectsNoIntersections(int[] inputA, int[] inputB)
+    {
+        var headA = NodeHelper.FromEnumerable(inputA);
+        var headB = NodeHelper.FromEnumerable(inputB);
+
+        var actual = Solutions.GetIntersectionNode(headA, headB);
+        Assert.Null(actual);
+    }
+}
---
+++ Chapters/Chapter02/Intersection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Shared.LinkedLists;
+
+namespace Chapters.Chapter02;
+/// <summary>
+/// Intersection: Given two (singly) linked lists, determine if the two lists intersect. Return the intersecting node.
+/// Note that the intersection is defined based on reference, not value. That is, if the kth node of the first linked
+/// list is the exact same node (by reference) as the jth node of the second linked list, then they are intersecting.
+/// </summary>
+public static partial class Solutions
+{
+    public static Node<T> GetIntersectionNode<T>(Node<T> headA, Node<T> headB)
+    {
+        throw new NotImplementedException();
+    }
+}
