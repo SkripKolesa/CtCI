152689b0252917833db82cc65b03475070678e7f
artem.novikov <biokako@gmail.com>
2024-05-13 23:36:45 +0400

    isPalindrome implementation

--- Chapters.Tests/Chapter02/PalindromeTests.cs
+++ Chapters.Tests/Chapter02/PalindromeTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Chapters.Chapter02;
 using Shared.LinkedLists;
 using Xunit;
 
@@ -8,17 +8,26 @@ public class PalindromeTests
 {
     [Theory]
     [InlineData(new[] { 1, 2, 3, 2, 1 })]
+    [InlineData(new[] { 1, 2, 2, 1 })]
+    [InlineData(new[] { 1, 2, 3, 3, 2, 1 })]
+    [InlineData(new[] { 1, 2, 3, 4, 3, 2, 1 })]
+    [InlineData(new[] { 1 })]
+    [InlineData(new[] { 1, 1 })]
     public void DetectsPalindromes(int[] input)
     {
         var head = NodeHelper.FromEnumerable(input);
-        throw new NotImplementedException();
+        Assert.True(Solutions.IsPalindrome(head));
     }
 
     [Theory]
     [InlineData(new[] { 1, 2, 3 })]
+    [InlineData(new[] { 1, 2, 2, 0 })]
+    [InlineData(new[] { 1, 2, 3, 3, 3, 1 })]
+    [InlineData(new[] { 1, 2, 3, 4, 3, 2, 0 })]
+    [InlineData(new[] { 1, 2 })]
     public void DetectsNonPalindromes(int[] input)
     {
         var head = NodeHelper.FromEnumerable(input);
-        throw new NotImplementedException();
+        Assert.False(Solutions.IsPalindrome(head));
     }
 }
--- Chapters/Chapter02/Palindrome.cs
+++ Chapters/Chapter02/Palindrome.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using Shared.LinkedLists;
 
 namespace Chapters.Chapter02;
 
+/// <summary>
+/// 2.6 Palindrome: Implement a function to check if a linked list is a palindrome.
+/// </summary>
 public static partial class Solutions
 {
     public static bool IsPalindrome<T>(Node<T> head)
     {
-        throw new NotImplementedException();
+        var current = head;
+        var endSeeker = current;
+        var compareBuffer = new Stack<T>();
+        var isLengthOdd = false;
+        while (endSeeker is not null)
+        {
+            compareBuffer.Push(current.Value);
+            current = current.Next;
+            if (endSeeker.Next is null)
+            {
+                isLengthOdd = true;
+            }
+            endSeeker = endSeeker.Next?.Next;
+        }
+
+        if (isLengthOdd)
+        {
+            compareBuffer.Pop();
+        }
+
+        while (current is not null)
+        {
+            if (!current.Value.Equals(compareBuffer.Pop()))
+            {
+                return false;
+            }
+            current = current.Next;
+        }
+
+        return true;
     }
 }
