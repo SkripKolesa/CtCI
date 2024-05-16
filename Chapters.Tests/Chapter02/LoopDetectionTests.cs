60ee315fcbf2c72cef69f5a2ba9bbd6f70e55058
artem.novikov <biokako@gmail.com>
2024-05-16 21:02:51 +0400

    loop detection implementation

---
+++ Chapters.Tests/Chapter02/LoopDetectionTests.cs
@@ -0,0 +1,56 @@
+﻿using Chapters.Chapter02;
+using Shared.LinkedLists;
+using Xunit;
+
+namespace Chapters.Tests.Chapter02;
+
+public class LoopDetectionTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 }, new[] { 10, 20, 30 })]
+    [InlineData(new[] { 0, 1 }, new[] { 2, 3, 4, 5, 6, 7, 8, 9 })]
+    [InlineData(new[] { 1, 2, 3 }, new[] { 10 })]
+    [InlineData(new[] { 1 }, new[] { 10 })]
+    [InlineData(new[] { 1 }, new[] { 10, 20 })]
+    [InlineData(new[] { 1, 2 }, new[] { 10, 20 })]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 10, 20, 30 })]
+    [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 10, 20, 30, 40 })]
+    public void DetectsLoop(int[] beginning, int[] loop)
+    {
+        var head = NodeHelper.FromEnumerable(beginning);
+        var loopStart = NodeHelper.FromEnumerable(loop);
+        var begTail = head;
+        while (begTail.Next != null)
+        {
+            begTail = begTail.Next;
+        }
+
+        begTail.Next = loopStart;
+        var loopTail = loopStart;
+        while (loopTail.Next != null)
+        {
+            loopTail = loopTail.Next;
+        }
+
+        loopTail.Next = loopStart;
+
+        var hasLoop = Solutions.TryGetLoopNode(head, out var actualNode);
+
+        Assert.True(hasLoop);
+        Assert.Same(loopStart, actualNode);
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 })]
+    [InlineData(new[] { 1, 2 })]
+    [InlineData(new[] { 1 })]
+    public void NotDetectsLoop(int[] input)
+    {
+        var head = NodeHelper.FromEnumerable(input);
+
+        var isLoop = Solutions.TryGetLoopNode(head, out var loopNode);
+
+        Assert.False(isLoop);
+        Assert.Null(loopNode);
+    }
+}
---
+++ Chapters/Chapter02/LoopDetection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Shared.LinkedLists;
+
+namespace Chapters.Chapter02;
+
+/// <summary>
+/// Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the beginning
+/// of the loop.
+/// DEFINITION
+/// Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to
+/// make a loop in the linked list.
+/// EXAMPLE
+/// Input: A -> B -> C -> D -> E -> C (the same C as earlier)
+/// Output: C
+/// </summary>
+public static partial class Solutions
+{
+    public static bool TryGetLoopNode<T>(Node<T> head, out Node<T> loopNode)
+    {
+        loopNode = null;
+
+        var runner = head;
+        var fastRunner = head.Next;
+        var isLoop = false;
+        int counter = 0;
+        while (runner is not null && fastRunner is not null)
+        {
+            counter++;
+            runner = runner.Next;
+            fastRunner = fastRunner?.Next?.Next;
+            if (fastRunner is null)
+            {
+                isLoop = false;
+                break;
+            }
+
+            if (fastRunner.Next == runner || fastRunner == runner || runner.Next == fastRunner)
+            {
+                isLoop = true;
+                break;
+            }
+        }
+
+        if (isLoop)
+        {
+            var stepsToTail = (counter + 1) / 2;
+            while (stepsToTail > 0)
+            {
+                runner = runner.Next;
+                stepsToTail--;
+            }
+
+            loopNode = runner.Next;
+        }
+
+        return isLoop;
+    }
+}
